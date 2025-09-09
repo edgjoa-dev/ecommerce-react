dc8f31a5f38c7eabf07e92fde11581c817b2330a
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2025-09-08 18:03:27 -0600

    feature/HU264-HomePage: Se añaden imagenes para el archivo de dataDummy.mock.ts

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,11 @@
+import { CustomPagination } from "@/components/custom"
 import { JumBotron } from "@/shop/components"
 
 export const HomePage = () => {
     return (
         <main className="min-h-screen">
             <JumBotron title="Página de inicio ecommerce" />
+            <CustomPagination totalPages={7}/>
         </main>
     )
 }
