60df6f028bd81f831cbc10476789e868e612dc24
Edgar Joaquin <floresedgarjoaquin@gmail.com>
2025-09-08 19:22:08 -0600

    Implementación de página de productos en HomePage desde dataDummy-mock e implemantación de sidebar mostrando filtros de tallas y precios

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import { CustomPagination } from "@/components/custom"
-import { JumBotron } from "@/shop/components"
+import { products } from "@/mock/productData.mock"
+import { JumBotron, ProductsGrid } from "@/shop/components"
 
 export const HomePage = () => {
     return (
         <main className="min-h-screen">
             <JumBotron title="Página de inicio ecommerce" />
+            <ProductsGrid products={products}  />
             <CustomPagination totalPages={7}/>
         </main>
     )
