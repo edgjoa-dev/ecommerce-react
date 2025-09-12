ef1e489c4b2ac53c68df8fd9b4bffe17160fdacf
Edgar Joaquin <floresedgarjoaquin@gmail.com>
2025-09-12 17:26:41 -0600

    UH271-DashboardNavigate: Se implementa la navegación en admin dshbord

--- src/admin/components/AdminSidebar.tsx
+++ src/admin/components/AdminSidebar.tsx
@@ -12,6 +12,7 @@ import {
   ChevronRight
 } from 'lucide-react';
 import { CustomLogo } from '@/components/custom';
+import { Link, useLocation } from 'react-router';
 
 interface SidebarProps {
   isCollapsed: boolean;
@@ -19,16 +20,27 @@ interface SidebarProps {
 }
 
 export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) => {
+
+  const { pathname } = useLocation();
+
+
   const menuItems = [
-    { icon: Home, label: 'Dashboard', active: true },
-    { icon: BarChart3, label: 'Analytics' },
-    { icon: Users, label: 'Users' },
-    { icon: ShoppingCart, label: 'Orders' },
-    { icon: FileText, label: 'Reports' },
-    { icon: Bell, label: 'Notifications' },
-    { icon: Settings, label: 'Settings' },
-    { icon: HelpCircle, label: 'Help' },
+    { icon: Home, label: 'Dashboard', to: '/admin' },
+    { icon: BarChart3, label: 'Productos', to: '/admin/products' },
+    { icon: Users, label: 'Usuarios' },
+    { icon: ShoppingCart, label: 'Ordenes' },
+    { icon: FileText, label: 'Reportes' },
+    { icon: Bell, label: 'Notificaciones' },
+    { icon: Settings, label: 'Configuraciones' },
+    { icon: HelpCircle, label: 'Ayuda' },
   ];
+
+
+  const isActiveRoute = (to: string ) => {
+    //TODO: ajustar cuando estemos en la pantalla de producto
+
+    return pathname === to
+  }
 
   return (
     <div className={`bg-white border-r border-gray-200 transition-all duration-300 ease-in-out ${isCollapsed ? 'w-16' : 'w-64'
@@ -51,9 +63,10 @@ export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) 
             const Icon = item.icon;
             return (
               <li key={index}>
-                <a
-                  href="#"
-                  className={`flex items-center space-x-3 px-3 py-2 rounded-lg transition-all duration-200 group ${item.active
+                <Link
+                  to={item.to || '/admin'}
+                  className={`flex items-center space-x-3 px-3 py-2 rounded-lg transition-all duration-200 group ${
+                    isActiveRoute(item.to || '/xxxxxx')
                       ? 'bg-blue-50 text-blue-600 border-r-2 border-blue-600'
                       : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900'
                     }`}
@@ -62,7 +75,7 @@ export const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) 
                   {!isCollapsed && (
                     <span className="font-medium">{item.label}</span>
                   )}
-                </a>
+                </Link>
               </li>
             );
           })}
